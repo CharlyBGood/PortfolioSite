63807744a3e559d6d2bc45c87b46426d766de65d
Carlos Bonavita <carlos.bonavita@dataart.com>
2023-08-25 20:00:20 -0300

    add preview form and some javascript for the call to action button

--- force-app/main/default/lwc/calltoactionButton/calltoactionButton.js
+++ force-app/main/default/lwc/calltoactionButton/calltoactionButton.js
@@ -1,10 +1,21 @@
 import { LightningElement } from 'lwc';
+import createLead from '@salesforce/apex/ContactMeController.createLead';
 
 export default class CalltoactionButton extends LightningElement {
   dialog;
+  firstName
+  lastName;
+  company;
+  email;
+  description;
 
   renderedCallback() {
     this.dialog = this.template.querySelector('.contact-dialog');
+    this.firstName = this.template.querySelector('.firstName');
+    this.lastName = this.template.querySelector('.lastName');
+    this.company = this.template.querySelector('.company');
+    this.email = this.template.querySelector('.email');
+    this.description = this.template.querySelector('.description');
   }
 
   showDialog() {
@@ -14,4 +25,21 @@ export default class CalltoactionButton extends LightningElement {
   closeDialog() {
     this.dialog.close();
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const firstNameVal = this.firstName.value;
+    const lastNameVal = this.lastName.value;
+    const companyVal = this.company.value;
+    const emailVal = this.email.value;
+    const descriptionVal = this.description.value;
+    createLead({
+      firstName: firstNameVal,
+      lastName: lastNameVal,
+      company: companyVal,
+      email: emailVal,
+      description: descriptionVal
+    });
+  }
+
 }
